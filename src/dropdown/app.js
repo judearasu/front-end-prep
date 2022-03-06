b8f1eb12bef0cb003e2410356646ab363b3356a1
judearasu <judearasu@gmail.com>
2022-03-06 08:36:34 -0500

    dropdown in draft

---
+++ src/dropdown/app.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('dom fully loaded');
+    let dropdown = document.querySelector('div#css-dropdown');
+    dropdown.classList.add('hide');
+
+    let button = document.querySelector('button#css-btn');
+    button.addEventListener('click', (event) => {
+        if (event?.srcElement?.ariaExpanded === 'false') {
+            dropdown.classList.remove('hide');
+            dropdown.classList.add('show');
+            button.setAttribute('aria-expanded', true);
+        } else {
+            dropdown.classList.remove('show');
+            dropdown.classList.add('hide');
+            button.setAttribute('aria-expanded', false);
+        }
+    })
+})
